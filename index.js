d39fa7122ec0eb4cf99964c3e8dac9cd4ef7be10
basheerkattan <basheerkattan2060@gmail.com>
2022-07-14 17:23:18 +0200

    finish

--- index.js
+++ index.js
@@ -20,45 +20,72 @@ numBtn.forEach(btn => {
       display1.innerText = inputNum.join('');
     } else if (calculation.length === 1 && inputNum2.length <= 6) {
       inputNum2.push(btn.innerText);
-      display1.innerText = inputNum.join('') + calculation + inputNum2.join("");
-
+      display1.innerText = inputNum.join('') + calculation + inputNum2.join('');
     }
   });
 });
 
- calc.forEach(btn =>{
-    btn.addEventListener('click', function () {
-        if(inputNum.length >0 && calculation.length === 0){
-            calculation = btn.innerText;
-            display1.innerText = inputNum.join("") + calculation;
-            
-        }
+calc.forEach(btn => {
+  btn.addEventListener('click', function () {
+    if(display2.innerText.length > 0 && inputNum.length === 0){
+        calculation = btn.innerText;
+      inputNum.push(display2.innerText);
+      display1.innerText = inputNum.join("") + calculation;
+
+    }
+    else if (inputNum.length > 0 && calculation.length === 0) {
+        calculation = btn.innerText;
+        display1.innerText = inputNum.join('') + calculation;
+      }
+  });
+});
+
+result.addEventListener('click', function () {
+  if (inputNum.length > 0 && calculation.length > 0 && inputNum2.length > 0) {
+    if (calculation === '+') {
+      res = Number(inputNum.join('')) + Number(inputNum2.join(''));
+      display2.innerText = res.toString();
+      
+    }else if(calculation === "-"){
+        res = Number(inputNum.join('')) - Number(inputNum2.join(''));
+        display2.innerText = res.toString();
         
-    });
+    }else if(calculation === "/"){
+        res = Number(inputNum.join('')) / Number(inputNum2.join(''));
+        display2.innerText = res.toString();
+        
+    }else if(calculation === "x"){
+        res = Number(inputNum.join('')) * Number(inputNum2.join(''));
+        display2.innerText = res.toString();
+        
+    }
+    inputNum = [];
+    calculation = "";
+    inputNum2 = [];
+  }
 });
 
-result.addEventListener('click', function(){
-    if(inputNum.length > 0 && calculation.length > 0 && inputNum2.length > 0){
-       if(calculation === "+"){
-         res = Math.floor( inputNum.join("")) + Math.floor( inputNum2.join("")) ;
-        display2.innerText = res
-        console.log(calculation);
-       }
+deleteBtn.addEventListener('click', function (){
+    if(calculation.length === 0){
+        display1.innerText = display1.innerText.toString().slice(0, -1);
+        inputNum.pop();
 
+    }else if(calculation.length === 1){
+        display1.innerText = display1.innerText.toString().slice(0, -1);
+        inputNum2.pop();
     }
+  
+
 });
 
-/* document.getElementById('clear').addEventListener('click', function (){
+clear.addEventListener('click', function(){
+  display1.innerText = "";
+  display2.innerText = "";
 
-}) 
+    inputNum = [];
+    calculation = "";
+    inputNum2 = [];
 
-/* class Calculator{
-    constructor(previous, current){
-        this.current = current;
-        this.previous = previous;
+});
 
-    }
 
-}
-
-const calculator = new Calculator() */
